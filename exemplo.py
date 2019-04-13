30387feb0b37ee6a7bd7a44ee8ef14ab45b2f1cb
Gabriel Gregório da Silva <48621655+gabriel-gregorio-da-silva@users.noreply.github.com>
2019-04-12 22:48:08 -0300

    Update exemplo.py

--- exemplo.py
+++ exemplo.py
@@ -3,6 +3,5 @@ from pyanalise import compare
 while True:
     a = input('Digite a primeira frase: ')
     b = input('Digite a segunda frase : ')
-    print("A semelhança entre '{}' e '{}' é de {}% \n".format(a,b,compare(a,b)))
-
+    print("A semelhança entre '{}' e '{}' é de {}% \n".format(a,b,compare.frase(a,b)))
 
